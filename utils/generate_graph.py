c16e8832ff1729e57542addcd67905374156af33
benjavides <benjavides@gmail.com>
2019-06-25 15:00:35 -0400

    Full image training graph generation

--- utils/generate_graph.py
+++ utils/generate_graph.py
@@ -1,12 +1,13 @@
 
+import matplotlib.pyplot as plt
 import matplotlib
 
 cuda = True
-#DATA
-avg_train_losses = [2.716564, 2.706419]
-avg_train_accuracies = [0, 0.4423924, 0.4459393]
-val_losses = [2.710972, 2.709453]
-val_accuracies = [0, 0.4491006, 0.44915591]
+# DATA
+avg_train_losses = [2.716564, 2.706419, 2.703597]
+avg_train_accuracies = [0, 0.4423924, 0.4459393, 0.4463724]
+val_losses = [2.710972, 2.709453, 2.712782]
+val_accuracies = [0, 0.4491006, 0.44915591, 0.448515]
 print(len(avg_train_losses))
 print(len(avg_train_accuracies))
 print(len(val_accuracies))
@@ -14,8 +15,6 @@ print(len(val_losses))
 
 if cuda:
     matplotlib.use('Agg')
-
-import matplotlib.pyplot as plt
 
 
 fig = plt.figure()
@@ -35,7 +34,8 @@ fig = plt.figure()
 fig.suptitle('Full Image Features Training', fontsize=20)
 plt.xlabel('epoch', fontsize=18)
 plt.ylabel('accuracy', fontsize=16)
-plt.plot(range(len(avg_train_accuracies)), avg_train_accuracies, 'b', label='train')
+plt.plot(range(len(avg_train_accuracies)),
+         avg_train_accuracies, 'b', label='train')
 plt.plot(range(len(val_accuracies)), val_accuracies, 'r', label='val')
 plt.legend(loc='best')
 
